38988d938313df71fd4643977b81275605ce9325
Marat-Tim <rurikcat@gmail.com>
2025-06-11 01:14:34 +0300

    не вычисляющие функции

--- common.py
+++ common.py
@@ -1,6 +1,16 @@
 import sympy as sp
-from sympy import Eq, Abs, sqrt, S
+from sympy import Eq, Abs, S, Mul, Pow
+from sympy import sqrt as Sqrt
 
 y = sp.Symbol('y')
 x = sp.Symbol('x')
 eps = 0.1
+
+def mul(*args):
+    return Mul(*args, evaluate=False)
+
+def div(a, b):
+    return Mul(a, Pow(b, -1, evaluate=False), evaluate=False)
+
+def sqrt(a):
+    return Sqrt(a, evaluate=False)
